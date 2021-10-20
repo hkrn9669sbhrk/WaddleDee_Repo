876aa502b3b17a5b65789388b8c44ae3763ad257
hkrn9669sbhrk <yasai69batake@gmail.com>
2021-10-20 17:47:23 +0900

    Add

---
+++ Template/C#/Auto_Var/Auto_Var.cs
@@ -0,0 +1,15 @@
+public class Test{
+    public static void Main(){
+		var    b_val = false;    // 論理値
+		var    i_val = 20000;   // 整数
+		var    d_val = 30.225;  // 実数
+		var    c_val = 'o';     // 文字
+		var    s_val = "words";  // 文字列
+ 
+		System.Console.WriteLine(b_val);
+		System.Console.WriteLine(i_val);
+		System.Console.WriteLine(d_val);
+		System.Console.WriteLine(c_val);
+		System.Console.WriteLine(s_val);
+    }
+}
---
+++ Template/C#/CS_Calc/CS_Calc.cs
@@ -0,0 +1,12 @@
+public class Test{
+    public static void Main(){
+		int val_1 = 10;
+		int val_2 = 5;
+
+		System.Console.WriteLine(val_1+val_2);
+		System.Console.WriteLine(val_1-val_2);
+		System.Console.WriteLine(val_1*val_2);
+		System.Console.WriteLine(val_1/val_2);
+		System.Console.WriteLine(val_1%val_2);
+    }
+}
