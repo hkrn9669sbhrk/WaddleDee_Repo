77079d3b6b85ea26c13dc0e79a1dba769fac7f16
hkrn9669sbhrk <yasai69batake@gmail.com>
2021-10-20 16:22:38 +0900

    Update AssemblyInfo.cs

--- Android_test/Android_test/Android_test/Properties/AssemblyInfo.cs
+++ Android_test/Android_test/Android_test/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Android_test")]
+[assembly: AssemblyTitle("あすクリ")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Android_test")]
+[assembly: AssemblyProduct("Ask_christmas")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
