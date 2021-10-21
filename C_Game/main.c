1b39d975028bb87cf565719722eed23b8e715f8a
hkrn9669sbhrk <yasai69batake@gmail.com>
2021-10-21 17:43:04 +0900

    Add

--- C_Game/main.c
+++ C_Game/main.c
@@ -10,13 +10,35 @@ int main(void)
 	int speed;
 	int defence;
 	int luk;
+	int damage;
 	float time;
 	
-	printf("こんにちは！僕はっくん！このゲームの製作者だよ！");
+	printf("こんにちは！僕はっくん！このゲ-ムの作者だよ！▽");
+	printf("\n");
 	getchar();
 	
-	printf("");
+	printf("事前に伝えておかなきゃいけない事があったから この場を借りて言わせてもらうよ！▽");
+	printf("\n");
 	getchar();
+	
+	printf("このゲ-ムやン-スコ-ドは完全なフリ-だよ！著作権を放棄しているよ！▽");
+	printf("\n");
+	getchar();
+	
+	printf("だから自作発言やン-ス改変も自由だよ！自由に使ってね！▽");
+	printf("\n");
+	getchar();
+	
+	printf("\n\n");
+	printf("※開発環境の仕様の為、伸ばし棒が使えません。ハイフンを代用しています。\n");
+	printf("※上記同理由の為、カタカナの'そ'が使えません。カタカナの'ん'を代用しています。\n");
+	printf("非常に見づらいと思いますが、上記二点の把握をお願い致します。");
+	getchar();
+	
+	
+	
+	
+	
 	
 	
 	return 0;
